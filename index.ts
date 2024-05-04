9e298d0c60dd7d0d7af3893cb444524d26b73d6a
hamzah-ayub <hamzah.ayub96@gmail.com>
2024-05-04 11:59:32 +0500

    First proper commit.

---
+++ index.ts
@@ -0,0 +1,29 @@
+import inquirer from "inquirer"
+
+const data = await inquirer.prompt([
+    {   message: "Enter string to count words: ",
+        type: "string",
+        name: "user_string"
+    }
+])
+
+//console.log(data.user_string)
+
+// remove any trailing or leading whitespaces:
+let str = data.user_string.trim()
+//console.log(str)
+
+// split the string to an array of words
+let words = str.split(/\s+/)
+//console.log(words)
+
+let wordCount = words.length
+console.log(`Word Count: ${wordCount}`)
+
+let chrStr = data.user_string.trim().replace(/\s+/g, '')
+//console.log(`Character String: ${chrStr}`)
+//console.log(typeof(chrStr))
+
+let characterCount: number = chrStr.length
+
+console.log(`Character Count: ${characterCount}`)
